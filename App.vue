11aa191268df7cddcd81a64034441c1f5b5597da
Will <will@onezerohosting.com>
2024-05-08 08:21:08 -0700

    .

--- App.vue
+++ App.vue
@@ -8,6 +8,20 @@
                 <ugh-main v-else :screenCapture="screenCapture" :auth="auth" @signin="signin" @signup="signup" />
             </v-container>
         </v-main>
+        <v-snackbar text :timeout="-1" v-model="snackbar.active" style="opacity: 0.9" @click="snackbarCloseHandler">
+            <v-row>
+                <v-col cols="1" class="d-flex align-center justify-center">
+                    <v-icon :icon="snackbar.icon" :color="snackbar.iconColor" />
+                </v-col>
+                <v-col cols="10" class="d-flex align-center justify-center">
+                    <span v-if="error">{{ snackbar.message }}</span>
+                    <span v-else @click="reload" class="font-weight-light" v-bind:class="smAndDown ? 'caption' : ''" style="cursor: pointer">App update available.</span>
+                </v-col>
+                <v-col cols="1" class="d-flex align-center justify-center">
+                    <v-btn variant="plain" :size="smAndDown ? 'x-small' : ''" @click="snackbarCloseHandler"> x </v-btn>
+                </v-col>
+            </v-row>
+        </v-snackbar>
     </v-app>
 </template>
 <script setup>
@@ -28,6 +42,16 @@ const extensionId = chrome?.runtime?.id || VITE_APP_EXTENSION_ID
 const route = ref({})
 const broadcast = ref()
 const store = useAppStore()
+const version = ref()
+const snackbarDefault = {
+    active: false,
+    icon: 'info',
+    message: undefined,
+}
+const snackbar = ref({ ...snackbarDefault })
+const lastBuild = ref()
+const versionCheckIntervalId = ref()
+const buildInfo = ref()
 
 async function doAuth(redirect) {
     await $keycloak.value.isLoaded
@@ -51,6 +75,37 @@ async function doAuth(redirect) {
         }
     }
 }
+const checkVersion = async () => {
+    buildInfo.value = await $api.buildInfo()
+
+    if (!buildInfo.value?.build_date) {
+        return
+    }
+    version.value = buildInfo.value.commit_sha
+    if (lastBuild.value && lastBuild.value?.build_date !== buildInfo.value.build_date) {
+        snackbar.value.active = true
+        // TODO: put some extra check here...
+        resetLocalStorage.value = true
+    } else {
+        lastBuild.value = buildInfo.value
+    }
+}
+function snackbarCloseHandler() {
+    snackbar.value.active = false
+    setTimeout(() => {
+        snackbar.value = { ...snackbarDefault }
+        if (error) {
+            error.value = false
+        }
+    })
+}
+function reload() {
+    const url = new URL(window.location.href)
+    url.searchParams.set('cache', Date.now())
+    window.location.href = url.toString()
+}
+checkVersion()
+versionCheckIntervalId.value = setInterval(checkVersion, 60000)
 const signin = () => $keycloak.value.login({ redirectUri: route.value.params?.get('redirect') || window.location.origin })
 const signup = () => $keycloak.value.login({ redirectUri: route.value.params?.get('redirect') || window.location.origin, action: 'register' })
 async function webShareInit() {
